998514ee34c914a7bd85222c68720943242e658f
Landini Alex <alex.landini@gmail.com>
2017-07-14 07:56:56 +0200

    http

--- src/app/shared/wikipedia.service.ts
+++ src/app/shared/wikipedia.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@angular/core';
-import {Jsonp, URLSearchParams} from "@angular/http";
+import {Injectable} from '@angular/core';
+import {Jsonp, Response, URLSearchParams} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class WikipediaService {
 
-  constructor(private jsonp: Jsonp) {}
+  constructor(private jsonp: Jsonp) {
+  }
 
-  search (term: string) {
+  search(term: string) {
 
     let wikiUrl = 'http://en.wikipedia.org/w/api.php';
 
@@ -19,8 +22,29 @@ export class WikipediaService {
 
     // TODO: Add error handling
     return this.jsonp
-      .get(wikiUrl, { search: params })
-      .map(response => <string[]> response.json()[1]);
+      .get(wikiUrl, {search: params})
+        .map(response => <string[]> response.json()[1])
+      //.map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  private extractData(response: Response) {
+    let body = response.json();
+    return body.data || {};
+  }
+
+  private handleError(error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
 }
